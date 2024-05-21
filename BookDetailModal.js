26bb6c5091386e756abd85b83dbf599613ad2bdc
pavelkarafiat <kasarar@seznam.cz>
2024-05-21 16:00:15 +0200

    update modal

--- BookDetailModal.js
+++ BookDetailModal.js
@@ -7,6 +7,7 @@ export default {
           <span class="close" @click="closeModal">&times;</span>
           <h2>{{ book.autor }}</h2>
           <h3>{{ book.nazev }}</h3>
+          <img :src="'img/' + book.imglink" @load="handleImageLoad($event)" loading="lazy">
           <p><strong>Read:</strong> {{ book.precteno }}</p>
           <p><strong>Published:</strong> {{ book.vydano }}</p>
           <p><strong>Description:</strong> {{ book.popis }}</p>
@@ -18,6 +19,9 @@ export default {
       closeModal() {
         this.$emit('close');
       },
+      handleImageLoad(event) {
+        event.target.style.opacity = 1;
+      }
     }
   };
 
